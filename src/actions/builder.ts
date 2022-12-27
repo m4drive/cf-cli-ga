243d99b5f3e29cf9f1309f39923a02a1fc11096f
Dmitrii Gritsai <gritsay.ds@gmail.com>
2022-12-27 14:34:07 +0300

    Change action builder logic to put actions  in separate folders under cf folder.

--- src/actions/builder.ts
+++ src/actions/builder.ts
@@ -1,11 +1,22 @@
-/* eslint-disable no-console */
 import yaml from 'js-yaml';
 import {Base} from '../github/Base';
+import fs from 'fs';
 import {DeployAction} from './definitions/deploy';
+import {InstallAction} from './definitions/install-cli';
 
-const actionsArray = [DeployAction];
-const SEPARATOR = '\r\n---\r\n\r\n';
+const actionsArray = [DeployAction, InstallAction];
+const BASE_LOCATION = './cf';
+/*const SEPARATOR = '\r\n---\r\n\r\n';
 const result = actionsArray
   .map((action: Base) => yaml.dump(action.getAsJsonObject()))
   .join(SEPARATOR);
-console.log(result);
+console.log(result);*/
+fs.rmSync(BASE_LOCATION, {recursive: true, force: true});
+fs.mkdirSync(BASE_LOCATION);
+actionsArray.map((action: Base) => {
+  fs.mkdirSync(`${BASE_LOCATION}/${action.getFolderName()}`);
+  fs.writeFileSync(
+    `${BASE_LOCATION}/${action.getFolderName()}/action.yaml`,
+    action.getAsYaml()
+  );
+});
