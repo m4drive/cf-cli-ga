4ab72b3766af3e7b11181adb42c79fa037024687
Dmitrii Gritsai <gritsay.ds@gmail.com>
2023-01-01 14:40:51 +0300

    Add listeners support

--- src/action/BaseAction.ts
+++ src/action/BaseAction.ts
@@ -115,16 +115,102 @@ export class BaseAction {
       core.setSecret(String(input.value));
     }
   }
+  protected stdOutListeners: ((data: Buffer) => void)[] = [];
+  protected stdErrListeners: ((data: Buffer) => void)[] = [];
+  protected stdLineListeners: ((data: string) => void)[] = [];
+  protected errLineListeners: ((data: string) => void)[] = [];
+  protected debugListeners: ((data: string) => void)[] = [];
+  protected endListeners: Function[] = [];
+
+  addListenerOnStdOut(listener: (data: Buffer) => void): void {
+    this.stdOutListeners.push(listener);
+  }
+  addListenerOnStdErr(listener: (data: Buffer) => void): void {
+    this.stdErrListeners.push(listener);
+  }
+  addListenerOnStdLine(listener: (data: string) => void): void {
+    this.stdLineListeners.push(listener);
+  }
+  addListenerOnErrLine(listener: (data: string) => void): void {
+    this.errLineListeners.push(listener);
+  }
+  addListenerOnDebug(listener: (data: string) => void): void {
+    this.debugListeners.push(listener);
+  }
+  addListenerOnEnd(listener: Function): void {
+    this.endListeners.push(listener);
+  }
+  async exec(
+    command: string,
+    parameters: string[] = [],
+    options: exec.ExecOptions = {}
+  ): Promise<number> {
+    if (this.getHideOutput()) {
+      options.silent = true;
+    }
+    if (!options.listeners) {
+      options.listeners = {};
+    }
+    if (options.listeners.debug)
+      this.addListenerOnDebug(options.listeners.debug);
+    if (options.listeners.errline)
+      this.addListenerOnErrLine(options.listeners.errline);
+    if (options.listeners.stderr)
+      this.addListenerOnStdErr(options.listeners.stderr);
+    if (options.listeners.stdline)
+      this.addListenerOnStdLine(options.listeners.stdline);
+    if (options.listeners.stdout)
+      this.addListenerOnStdOut(options.listeners.stdout);
+
+    if (this.debugListeners.length > 0) {
+      options.listeners.debug = (data: string) => {
+        for (const listener of this.debugListeners) {
+          listener(data);
+        }
+      };
+    }
+    if (this.errLineListeners.length > 0) {
+      options.listeners.errline = (data: string) => {
+        for (const listener of this.errLineListeners) {
+          listener(data);
+        }
+      };
+    }
+    if (this.stdErrListeners.length > 0) {
+      options.listeners.stderr = (data: Buffer) => {
+        for (const listener of this.stdErrListeners) {
+          listener(data);
+        }
+      };
+    }
+    if (this.stdLineListeners.length > 0) {
+      options.listeners.stdline = (data: string) => {
+        for (const listener of this.stdLineListeners) {
+          listener(data);
+        }
+      };
+    }
+    if (this.stdOutListeners.length > 0) {
+      options.listeners.stdout = (data: Buffer) => {
+        for (const listener of this.stdOutListeners) {
+          listener(data);
+        }
+      };
+    }
+
+    const result = await exec.exec(command, parameters, options);
+    for (const listener of this.endListeners) {
+      listener();
+    }
+    return result;
+  }
+
   async run(): Promise<void> {
     try {
       await this.getInput();
       this.hideSecrets(this.inputs);
       const command = await this.buildCommand(this.inputs);
-      if (this.getHideOutput()) {
-        await exec.exec(command, [], {silent: true});
-      } else {
-        await exec.exec(command);
-      }
+      await this.exec(command);
     } catch (exception) {
       core.setFailed(String(exception));
     }
