1382fd35c4c395247f8fda4146171d4ae77ae39f
Dmitrii Gritsai <gritsay.ds@gmail.com>
2023-01-02 17:52:58 +0300

    Configure deploy action inputs

--- src/action-metadata/definitions/deploy.ts
+++ src/action-metadata/definitions/deploy.ts
@@ -1,6 +1,5 @@
 import {UsingEnum} from '../../action-metadata-schema/ActionRuns';
 import {Action} from '../../action-metadata-schema/Action';
-import {InstallationInputs} from './blocks/installationInputs';
 import {LoginInputs} from './blocks/loginInputs';
 
 export const DeployAction = new Action({
@@ -9,11 +8,110 @@ export const DeployAction = new Action({
   author: 'Your name or organization here',
   inputs: {
     ...LoginInputs,
-    ...InstallationInputs,
     ...{
       mtaPath: {
-        required: true,
+        required: false,
         description: 'path to mta'
+      },
+      mtaFile: {
+        required: false,
+        description: ' '
+      },
+      modules: {
+        required: false,
+        description: ' '
+      },
+      resources: {
+        required: false,
+        description: ' '
+      },
+      operationId: {
+        required: false,
+        description: ' '
+      },
+      action: {
+        required: false,
+        description: ' '
+      },
+      extDescriptor: {
+        required: false,
+        description: ' '
+      },
+      timeout: {
+        required: false,
+        description: ' '
+      },
+      versionRule: {
+        required: false,
+        description: ' '
+      },
+      deployServiceUrl: {
+        required: false,
+        description: ' '
+      },
+      force: {
+        required: false,
+        description: ' '
+      },
+      retries: {
+        required: false,
+        description: ' '
+      },
+      noStart: {
+        required: false,
+        description: ' '
+      },
+      namespace: {
+        required: false,
+        description: ' '
+      },
+      deleteService: {
+        required: false,
+        description: ' '
+      },
+      deleteServiceKeys: {
+        required: false,
+        description: ' '
+      },
+      deleteServiceBrokers: {
+        required: false,
+        description: ' '
+      },
+      keepFiles: {
+        required: false,
+        description: ' '
+      },
+      noRestartSubscribedApps: {
+        required: false,
+        description: ' '
+      },
+      doNotFailOnMissingPermissions: {
+        required: false,
+        description: ' '
+      },
+      abortOnError: {
+        required: false,
+        description: ' '
+      },
+      verifyArchiveSignature: {
+        required: false,
+        description: ' '
+      },
+      strategy: {
+        required: false,
+        description: ' '
+      },
+      skipTestingPhase: {
+        required: false,
+        description: ' '
+      },
+      skipIdleStart: {
+        required: false,
+        description: ' '
+      },
+      saveDMOLOn: {
+        required: false,
+        description: ' '
       },
       flags: {
         required: false,
